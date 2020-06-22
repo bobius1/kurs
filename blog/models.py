f359b9ce9b16d109ba0dec7319b34929df6abd2c
bobius1 <zloykazanec@gmail.com>
2020-06-22 19:03:45 +0300

    add model Category

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,10 @@ class Category(models.Model):
     """Модель категорий"""
     name = models.CharField(verbose_name='Имя', max_length=100)
     slug = models.SlugField(verbose_name='url', max_length=100)
-
+    """Возвращает имя категории"""
     def __str__(self):
         return self.name
-
+    """Задаём имя в единственном и множественном числе"""
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+from .admin import Category
+
+"""методом функций"""
+
+# def home(request):
+#     if request.method == "POST":
+#         return HttpResponse("Hi")
+#     elif request.method == "GET":
+#         return HttpResponse("Good")
+
+"""метод класса"""
+
+# class HomeView(View):
+#     def get(self, request):
+#         return HttpResponse("Good")
+#
+#     def post(self):
+#         pass
+
+class HomeView(View):
+    """Home page"""
+    def get(self, request):
+        category_list = Category.objects.all()
+        print(category_list)
+        return render(request, "blog/home.html", {"categories":category_list})
+
+class CategoryView(View):
+    """Вывод статей категории"""
+    def get(self, request, category_name):
+        category = Category.objects.get(slug=category_name)
+        return render(request, "blog/post_list.html", {"category": category })
+
+
+
+
+
